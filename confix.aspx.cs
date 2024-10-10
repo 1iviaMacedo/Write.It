7780a975976bb3532e16476c52e4c36e0d35383b
Lívia Macedo <125596597+1iviaMacedo@users.noreply.github.com>
2024-10-10 07:53:02 -0300

    Create confix.aspx.cs

---
+++ confix.aspx.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _2024._08._27
+{
+    public partial class config : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            txtNome.Visible = true;
+        }
+
+        protected void Button7_Click1(object sender, EventArgs e)
+        {
+            txtEmail.Visible = true;
+        }
+
+        protected void Button8_Click1(object sender, EventArgs e)
+        {
+            txtSenha.Visible = true;
+        }
+
+        protected void Button9_Click1(object sender, EventArgs e)
+        {
+            txtBio.Visible = true;
+        }
+
+        protected void txtNome_TextChanged(object sender, EventArgs e)
+        {
+            string 
+        }
+    }
+}
